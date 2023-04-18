d6e44b59d709c931926d440e5937fa76aef1d32a
Miguel <miguel.angel2007.gonzalez@gmail.com>
2023-04-17 22:14:48 -0500

    GUI - Ventana Principal,Instrucciones y Categoria

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+import GUI.VentanaPrincipal;
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Miguel
+ */
+public class Main extends JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+        ventanaPrincipal.setVisible(true);
+        ventanaPrincipal.setLocationRelativeTo(null);
+    }
+    
+}
